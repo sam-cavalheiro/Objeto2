b32cd9d76314f44c5820d6af2a066ec8132eb172
4larts <alissoncdoprado56@gmail.com>
2021-07-31 18:55:42 -0300

    grilo modificado

--- src/Grilo.java
+++ src/Grilo.java
@@ -1,19 +1,34 @@
 public class Grilo{
     
-    private String id_grilo;
-    private int tamanho_pulo;
-    private int total_pulo;
-    private int caminho_percorrido;
-    private int contador_tempo;
-    private int distancia_chegada;
+    private String idGrilo;
+    private int tamanhoPulo;
+    private int totalPulo;
+    private int caminhoPercorrido;
+    private int contadorTempo = 2;
+    private int distanciaChegada;
     boolean chegada = true;
-    public Grilo(String id_grilo) {
-    	this.id_grilo = id_grilo;
+  
+    public Grilo(String idGrilo) {
+    	this.idGrilo = idGrilo;
     }
     
     public void atualiza() 
     { 
-        this.contador_tempo -=1;
+        this.contadorTempo -=1;
+        
+        if(contadorTempo <= 0)
+             {
+                totalPulo +=1;
+                caminhoPercorrido = caminhoPercorrido+tamanhoPulo;
+                                System.out.println("Grilo_"+idGrilo+"_pulou_"+tamanhoPulo+"_e_já_percorreu_"+caminhoPercorrido+".");
+                if (caminhoPercorrido>=distanciaChegada)
+                {
+                    System.out.println("Grilo_"+idGrilo+"chegou_na_linha_de_chegada_com_"+totalPulo+"_pulos.");
+                }
+                else
+                {
+                //redefinir contador
+                }
+             }
     }
-
 }
