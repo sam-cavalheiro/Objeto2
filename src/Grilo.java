d66adf052e39da24f172052ee353498047081f89
Sam Cavalheiro <sam@pop-os.localdomain>
2021-07-31 18:46:40 -0300

    Thread adicionado à classe Grilo

--- src/Grilo.java
+++ src/Grilo.java
@@ -1,4 +1,4 @@
-public class Grilo{
+public class Grilo implements Runnable {
     
     private String id_grilo;
     private int tamanho_pulo;
@@ -6,14 +6,23 @@ public class Grilo{
     private int caminho_percorrido;
     private int contador_tempo;
     private int distancia_chegada;
-    boolean chegada = true;
+    boolean chegada = false;
     public Grilo(String id_grilo) {
     	this.id_grilo = id_grilo;
+    	run();
     }
     
     public void atualiza() 
     { 
+    	System.out.println("Testando");
         this.contador_tempo -=1;
     }
+
+	@Override
+	public void run() {
+		while (!chegada) {
+			atualiza();
+		}
+	}
 
 }
