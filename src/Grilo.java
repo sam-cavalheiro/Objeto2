e7c56332f0e174f9027e02efe1e687163d283413
LucasLaport <llucaslaport@gmail.com>
2021-07-31 21:10:52 -0300

    metodo de redefinição de tempo e pulo

--- src/Grilo.java
+++ src/Grilo.java
@@ -1,5 +1,12 @@
+import java.util.Random;
 public class Grilo implements Runnable {
     
+	private static final int TAMANHO_MINIMO_PULO = 1;
+	private static final int TAMANHO_MAXIMO_PULO = 10;
+	
+	private static final int RESETAR_TEMPO_MINIMO = 500;
+	private static final int RESETAR_TEMPO_MAXIMO = 3000;
+	
     private String idGrilo;
     private int tamanhoPulo;
     private int totalPulo;
@@ -11,6 +18,7 @@ public class Grilo implements Runnable {
     private Thread thread;
 
     public Grilo(String idGrilo) {
+    	reseta();
     	this.idGrilo = idGrilo;
     	thread = new Thread(this, idGrilo);
     	//run();
@@ -32,10 +40,22 @@ public class Grilo implements Runnable {
                 }
                 else
                 {
+                	reseta();
+                	
                 //redefinir contador
                 	
                 }
              }
+    }
+    
+    private void reseta()
+    {
+    	Random random = new Random();
+		
+		
+    	tamanhoPulo = TAMANHO_MINIMO_PULO + random.nextInt(TAMANHO_MAXIMO_PULO - TAMANHO_MINIMO_PULO);
+    	
+    	contadorTempo = RESETAR_TEMPO_MINIMO + random.nextInt(RESETAR_TEMPO_MAXIMO - RESETAR_TEMPO_MINIMO);
     }
 
 	@Override
